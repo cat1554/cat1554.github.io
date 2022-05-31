838b2de95693ac26f1300eede58ea9540d8dc3e3
cat1554 <arthurkrug@icloud.com>
2022-05-31 10:41:14 -0500

    Update page.js

--- tlw_test/asset/game/mc/metro/page.js
+++ tlw_test/asset/game/mc/metro/page.js
@@ -51,7 +51,6 @@ function getObjectFitSize(
 }
 
 function mapRenderLine(x, z, clr){
-//	window.console.log(x.x, x.z, z.x, z.z, clr);
 	ctx.beginPath();
 	ctx.lineWidth = Math.max((pan_y * 16), 16);
 	ctx.strokeStyle = clr;
@@ -66,126 +65,15 @@ function mapRenderLine(x, z, clr){
 	ctx.stroke();
 }
 
+function mapRenderStop(x, z, dat){
+}
+
 function mapRenderStep(){
 	for (let lineLoop = 0; lineLoop < xmlobj.lines.length; lineLoop++) {
 		for (let lineRenderLoop = 0; lineRenderLoop < (xmlobj.lines[lineLoop].points.length - 1); lineRenderLoop++) {
 			mapRenderLine(xmlobj.lines[lineLoop].points[lineRenderLoop], xmlobj.lines[lineLoop].points[lineRenderLoop + 1], xmlobj.lines[lineLoop].colour);
 		}
 	}
-
-/*	clockable = new Date();
-
-	ctx.beginPath();
-	ctx.arc((75 * clockScaleMultiplier), (75 * clockScaleMultiplier), (52 * clockScaleMultiplier), 0, (2 * Math.PI));
-	ctx.fillStyle = "black";
-	ctx.fill();
-
-	clockable_secval = clockable.getSeconds() + (clockable.getMilliseconds() / 1000);
-
-	if ((Math.round( (6 - clockable_secval) * 255)) > 254.5) {
-		my_gradient_fade = "FF";
-	} else if ((Math.round( (6 - clockable_secval) * 255)) < 0.5) {
-		my_gradient_fade = "00";
-	} else if ((Math.round( (6 - clockable_secval) * 255)) < 15.5) {
-		my_gradient_fade = "0" + ((Math.round( (6 - clockable_secval) * 255)).toString(16));
-	} else {
-		my_gradient_fade = ((Math.round( (6 - clockable_secval) * 255)).toString(16));
-	}
-
-	ctx.beginPath();
-	ctx.arc((75 * clockScaleMultiplier), (75 * clockScaleMultiplier), (50 * clockScaleMultiplier), 0, (2 * Math.PI));
-
-	my_gradient = ctx.createLinearGradient(((-100 + (clockable.getMilliseconds() / 10)) * clockScaleMultiplier), ((-75 + (clockable.getMilliseconds() / 10)) * clockScaleMultiplier), ((100 + (clockable.getMilliseconds() / 10)) * clockScaleMultiplier), ((125 + (clockable.getMilliseconds() / 10)) * clockScaleMultiplier));
-	my_gradient.addColorStop(0, "#FF0000" + my_gradient_fade);
-	my_gradient.addColorStop((1/24), "#FFFF00" + my_gradient_fade);
-	my_gradient.addColorStop((2/24), "#00FF00" + my_gradient_fade);
-	my_gradient.addColorStop((3/24), "#00FFFF" + my_gradient_fade);
-	my_gradient.addColorStop((4/24), "#0000FF" + my_gradient_fade);
-	my_gradient.addColorStop((5/24), "#FF00FF" + my_gradient_fade);
-	my_gradient.addColorStop((6/24), "#FF0000" + my_gradient_fade);
-	my_gradient.addColorStop((7/24), "#FFFF00" + my_gradient_fade);
-	my_gradient.addColorStop((8/24), "#00FF00" + my_gradient_fade);
-	my_gradient.addColorStop((9/24), "#00FFFF" + my_gradient_fade);
-	my_gradient.addColorStop((10/24), "#0000FF" + my_gradient_fade);
-	my_gradient.addColorStop((11/24), "#FF00FF" + my_gradient_fade);
-	my_gradient.addColorStop((12/24), "#FF0000" + my_gradient_fade);
-
-	my_gradient.addColorStop((13/24), "#FFFF00" + my_gradient_fade);
-	my_gradient.addColorStop((14/24), "#00FF00" + my_gradient_fade);
-	my_gradient.addColorStop((15/24), "#00FFFF" + my_gradient_fade);
-	my_gradient.addColorStop((16/24), "#0000FF" + my_gradient_fade);
-	my_gradient.addColorStop((17/24), "#FF00FF" + my_gradient_fade);
-	my_gradient.addColorStop((18/24), "#FF0000" + my_gradient_fade);
-	my_gradient.addColorStop((19/24), "#FFFF00" + my_gradient_fade);
-	my_gradient.addColorStop((20/24), "#00FF00" + my_gradient_fade);
-	my_gradient.addColorStop((21/24), "#00FFFF" + my_gradient_fade);
-	my_gradient.addColorStop((22/24), "#0000FF" + my_gradient_fade);
-	my_gradient.addColorStop((23/24), "#FF00FF" + my_gradient_fade);
-	my_gradient.addColorStop(1, "#FF0000" + my_gradient_fade);
-
-	ctx.fillStyle = my_gradient;
-	ctx.fill();
-
-	ctx.beginPath();
-	ctx.fillStyle = "#FFA500FF";
-	ctx.arc((75 * clockScaleMultiplier), (75 * clockScaleMultiplier), (50 * clockScaleMultiplier), (-0.5 * Math.PI), (((clockable_secval / 30) - 0.5) * Math.PI));
-	ctx.lineTo((75 * clockScaleMultiplier), (75 * clockScaleMultiplier));
-	ctx.fill();
-
-	ctx.beginPath();
-	if(clockable_secval > 0.5) {
-		ctx.fillStyle = "#FFA50000";
-	} else {
-		if((255 - (Math.round(clockable_secval * 510))) >= 16) {
-			ctx.fillStyle = "#FFA500" + (255 - (Math.round(clockable_secval * 510))).toString(16);
-		} else {
-			ctx.fillStyle = "#FFA5000" + (255 - (Math.round(clockable_secval * 510))).toString(16);
-		}
-	}
-	ctx.arc((75 * clockScaleMultiplier), (75 * clockScaleMultiplier), (50 * clockScaleMultiplier), 0, (2 * Math.PI));
-	ctx.lineTo((75 * clockScaleMultiplier), (75 * clockScaleMultiplier));
-	ctx.fill();
-
-	ctx.beginPath();
-	ctx.fillStyle = "#00000088";
-	ctx.moveTo((40 * clockScaleMultiplier), (65 * clockScaleMultiplier));
-	ctx.lineTo((100 * clockScaleMultiplier), (65 * clockScaleMultiplier));
-	ctx.arcTo((120 * clockScaleMultiplier), (65 * clockScaleMultiplier), (120 * clockScaleMultiplier), (70 * clockScaleMultiplier), (10 * clockScaleMultiplier));
-	ctx.lineTo((120 * clockScaleMultiplier), (80 * clockScaleMultiplier));
-	ctx.arcTo((120 * clockScaleMultiplier), (85 * clockScaleMultiplier), (110 * clockScaleMultiplier), (85 * clockScaleMultiplier), (10 * clockScaleMultiplier));
-	ctx.lineTo((40 * clockScaleMultiplier), (85 * clockScaleMultiplier));
-	ctx.arcTo((30 * clockScaleMultiplier), (85 * clockScaleMultiplier), (30 * clockScaleMultiplier), (80 * clockScaleMultiplier), (10 * clockScaleMultiplier));
-	ctx.lineTo((30 * clockScaleMultiplier), (70 * clockScaleMultiplier));
-	ctx.arcTo((30 * clockScaleMultiplier), (65 * clockScaleMultiplier), (40 * clockScaleMultiplier), (65 * clockScaleMultiplier), (10 * clockScaleMultiplier));
-	ctx.fill();
-
-	if ((clockable.getHours() % 24) < 9.5) {
-		topbarhour = "0" + (clockable.getHours() % 24);
-	} else {
-		topbarhour = (clockable.getHours() % 24);
-	}
-	if ((clockable.getMinutes()) < 9.5) {
-		topbarminute = "0" + (clockable.getMinutes());
-	} else {
-		topbarminute = (clockable.getMinutes());
-	}
-	if ((clockable.getSeconds()) < 9.5) {
-		topbarsecond = "0" + (clockable.getSeconds());
-	} else {
-		topbarsecond = (clockable.getSeconds());
-	}
-
-	ctx.font = "900 " + (15 * clockScaleMultiplier) + "px monospace";
-	ctx.fillStyle="#FFF";
-	ctx.textAlign = "center";
-	ctx.fillText(topbarhour + " " + topbarminute + " " + topbarsecond, ((c.width/2)), ((c.height/2) + (6 * clockScaleMultiplier)));
-
-	if ((Math.round(((Math.sin((clockable_secval * 2) * Math.PI) + 1) / 2) * 255)) < 15.5) {
-		ctx.fillStyle="#FFFFFF0" + ((Math.round(((Math.sin((clockable_secval * 2) * Math.PI) + 1) / 2) * 255)).toString(16));
-	} else {
-		ctx.fillStyle="#FFFFFF" + ((Math.round(((Math.sin((clockable_secval * 2) * Math.PI) + 1) / 2) * 255)).toString(16));
-	}
-	ctx.fillText(":  :", ((c.width/2)), ((c.height/2) + (6 * clockScaleMultiplier))); */
 }
 
 const xmlhttp = new XMLHttpRequest();
@@ -194,7 +82,7 @@ xmlhttp.onload = function() {
 	window.console.log(myObj);
 	xmlobj = myObj;
 
-	setInterval(mapRenderStep, 100);
+	setInterval(mapRenderStep, 500);
 };
 
 function initPage(){
