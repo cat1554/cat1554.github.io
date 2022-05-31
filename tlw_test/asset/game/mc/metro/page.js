63e0769ba4abad4cacf7c9593000bdb95d3804b9
cat1554 <arthurkrug@icloud.com>
2022-05-31 10:07:46 -0500

    Update page.js

--- tlw_test/asset/game/mc/metro/page.js
+++ tlw_test/asset/game/mc/metro/page.js
@@ -58,7 +58,8 @@ function mapRenderStep(){
 	window.console.log("step");
 	for (let lineLoop = 0; lineLoop < xmlobj.lines.length; lineLoop++) {
 		window.console.log("lineLoop=" + lineLoop);
-		window.console.log("lineRenderLoopLimit=" + xmlobj.lines[lineLoop]);
+		window.console.log(xmlobj.lines[lineLoop]);
+		window.console.log(xmlobj.lines[lineLoop].length);
 		for (let lineRenderLoop = 0; lineRenderLoop < (xmlobj.lines[lineLoop].length - 1); lineRenderLoop++) {
 			window.console.log("lineRenderLoop=" + lineRenderLoop);
 			mapRenderLine(xmlobj.lines[lineRenderLoop][lineRenderLoop], xmlobj.lines[lineRenderLoop][lineRenderLoop + 1], xmlobj.lines[lineRenderLoop].colour);
