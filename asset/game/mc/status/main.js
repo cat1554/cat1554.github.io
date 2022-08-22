56e9edc3e0bd0cd7a618a2e5c3ca8df4563dd6cd
cat1554 <arthurkrug@icloud.com>
2022-08-22 00:34:12 -0500

    Update main.js

--- asset/game/mc/status/main.js
+++ asset/game/mc/status/main.js
@@ -16,7 +16,7 @@ function xmlGrab(){
 	for (let i = 0; i < 7; i++) {
 		if (("p" + (i + 1) + "n") !== svrxml.players[i].name) {
 			flipPlayer((i + 1), "/asset/game/mc/status/placeholder.png", svrxml.players[i].name, svrxml.players[i].rank, svrxml.players[i].ontime, svrxml.players[i].total);
-			("p" + (i + 1) + "n") = svrxml.players[i].name;
+			window["p" + (i + 1) + "n"] = svrxml.players[i].name;
 		} else {
 			updatePlayer((i + 1), "/asset/game/mc/status/placeholder.png", svrxml.players[i].name, svrxml.players[i].rank, svrxml.players[i].ontime, svrxml.players[i].total);
 		}
