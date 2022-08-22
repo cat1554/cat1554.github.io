312553d82d3941df181e3961f180e069a60ddcdd
cat1554 <arthurkrug@icloud.com>
2022-08-21 23:32:38 -0500

    aaaa

--- asset/game/mc/status/main.js
+++ asset/game/mc/status/main.js
@@ -18,70 +18,70 @@ function pleaseExist() {
 		document.getElementById("sv_players_o").style.color = "#F00";
 		document.getElementById("sv_players_m").style.color = "#F00";
 		document.getElementById("sv_players_p").style.color = "#F00";
-		document.getElementById("sv_players_g").style.right = "0%";
+		document.getElementById("sv_players_g").style.left = "100%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "7") {
 		document.getElementById("sv_players_c").style.color = "#F50";
 		document.getElementById("sv_players_o").style.color = "#F50";
 		document.getElementById("sv_players_m").style.color = "#F50";
 		document.getElementById("sv_players_p").style.color = "#F50";
-		document.getElementById("sv_players_g").style.right = "12.5%";
+		document.getElementById("sv_players_g").style.left = "87.5%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "6") {
 		document.getElementById("sv_players_c").style.color = "#F90";
 		document.getElementById("sv_players_o").style.color = "#F90";
 		document.getElementById("sv_players_m").style.color = "#F90";
 		document.getElementById("sv_players_p").style.color = "#F90";
-		document.getElementById("sv_players_g").style.right = "25%";
+		document.getElementById("sv_players_g").style.left = "75%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "5") {
 		document.getElementById("sv_players_c").style.color = "#FF0";
 		document.getElementById("sv_players_o").style.color = "#FF0";
 		document.getElementById("sv_players_m").style.color = "#FF0";
 		document.getElementById("sv_players_p").style.color = "#FF0";
-		document.getElementById("sv_players_g").style.right = "37.5%";
+		document.getElementById("sv_players_g").style.left = "62.5%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "4") {
 		document.getElementById("sv_players_c").style.color = "#9F5";
 		document.getElementById("sv_players_o").style.color = "#9F5";
 		document.getElementById("sv_players_m").style.color = "#9F5";
 		document.getElementById("sv_players_p").style.color = "#9F5";
-		document.getElementById("sv_players_g").style.right = "50%";
+		document.getElementById("sv_players_g").style.left = "50%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "3") {
 		document.getElementById("sv_players_c").style.color = "#5F9";
 		document.getElementById("sv_players_o").style.color = "#5F9";
 		document.getElementById("sv_players_m").style.color = "#5F9";
 		document.getElementById("sv_players_p").style.color = "#5F9";
-		document.getElementById("sv_players_g").style.right = "62.5%";
+		document.getElementById("sv_players_g").style.left = "37.5%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "2") {
 		document.getElementById("sv_players_c").style.color = "#0FF";
 		document.getElementById("sv_players_o").style.color = "#0FF";
 		document.getElementById("sv_players_m").style.color = "#0FF";
 		document.getElementById("sv_players_p").style.color = "#0FF";
-		document.getElementById("sv_players_g").style.right = "75%";
+		document.getElementById("sv_players_g").style.left = "25%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "1") {
 		document.getElementById("sv_players_c").style.color = "#0FF";
 		document.getElementById("sv_players_o").style.color = "#0FF";
 		document.getElementById("sv_players_m").style.color = "#0FF";
 		document.getElementById("sv_players_p").style.color = "#0FF";
-		document.getElementById("sv_players_g").style.right = "87.5%";
+		document.getElementById("sv_players_g").style.left = "12.5%";
 
 	} else if (document.getElementById("sv_players_c").innerHTML == "0") {
 		document.getElementById("sv_players_c").style.color = "#0FF";
 		document.getElementById("sv_players_o").style.color = "#0FF";
 		document.getElementById("sv_players_m").style.color = "#0FF";
 		document.getElementById("sv_players_p").style.color = "#0FF";
-		document.getElementById("sv_players_g").style.right = "100%";
+		document.getElementById("sv_players_g").style.left = "0%";
 
 	} else {
 		document.getElementById("sv_players_c").style.color = "#555";
 		document.getElementById("sv_players_o").style.color = "#555";
 		document.getElementById("sv_players_m").style.color = "#555";
 		document.getElementById("sv_players_p").style.color = "#555";
-		document.getElementById("sv_players_g").style.right = "100%";
+		document.getElementById("sv_players_g").style.left = "0%";
 	}
 
 }
@@ -101,9 +101,7 @@ xmlhttp.onerror = function() {
 };
 
 function initPage() {
-	
 	setTimeout(initPage2, 100);
-
 
 	xmlhttp.open("GET", "./../../../asset/game/mc/status/stat.json");
 	xmlhttp.send();
